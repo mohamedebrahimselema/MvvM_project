566f05a3031b8409289506a89840482572bbb428
your.user.name <35867025+mohamedebrahimselema@users.noreply.github.com>
2020-06-25 11:06:49 +0200

    implement authlistner in authviewmodel

--- app/src/main/java/com/example/mvvmproject/Ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/mvvmproject/Ui/auth/AuthViewModel.kt
@@ -7,13 +7,14 @@ class AuthViewModel : ViewModel() {
 
     var email : String? = null
     var password : String? = null
-
+    var iauthListner : IAuthListner ?=null
     fun onLoginBtnClicked(view: View){
 
         if(email.isNullOrEmpty() || password.isNullOrEmpty()){
+            iauthListner?.onFail("Invalid email or password")
             return
         }
         //sucess
-
+        iauthListner?.onSuccess()
     }
 }
